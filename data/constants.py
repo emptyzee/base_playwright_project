3ff183044a9d4bdf494fd763e33a1ddf32737337
behterevav <92896866+emptyzee@users.noreply.github.com>
2023-09-23 01:09:29 +0300

    Update constants.py

--- data/constants.py
+++ data/constants.py
@@ -2,18 +2,8 @@ import os
 
 
 class Constants:
-    TEACHER_DATA = {
-        "login_shot": "teacher5@uchi.ru",
-        "password_shot": "12345678"
-    }
-
     try:
         login2 = os.getenv('PROD_LOGIN1')
         password2 = os.getenv('PROD_PW1')
     except KeyError:
         print("LOGIN OR PW WASN'T FOUND")
-
-    try:
-        key = os.getenv('API_KEY')
-    except KeyError:
-        print("API KEY wasn't found")
